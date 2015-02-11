bf3ce558735beff6d7ba67f910bd846da433cb8f
ZhangYawei <zhangyawei@dhcp-164-107-20-198.osuwireless.ohio-state.edu>
2015-02-10 19:05:53 -0500

    Initial Commit

---
+++ practiceCocoapods/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  practiceCocoapods
+//
+//  Created by ZhangYawei on 2/10/15.
+//  Copyright (c) 2015 001. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ practiceCocoapodsTests/practiceCocoapodsTests.m
@@ -0,0 +1,40 @@
+//
+//  practiceCocoapodsTests.m
+//  practiceCocoapodsTests
+//
+//  Created by ZhangYawei on 2/10/15.
+//  Copyright (c) 2015 001. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface practiceCocoapodsTests : XCTestCase
+
+@end
+
+@implementation practiceCocoapodsTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
